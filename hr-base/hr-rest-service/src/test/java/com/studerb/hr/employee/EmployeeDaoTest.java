e2b65c6facb6b779dff2b0a350381a90938c3fdb
stbill79 <stbill79@1cf645f8-9684-11dd-b7a0-47679018daad>
2009-12-18 08:33:55 +0000

    got Hibernate to work

--- hr-base/hr-rest-service/src/test/java/com/studerb/hr/employee/EmployeeDaoTest.java
+++ hr-base/hr-rest-service/src/test/java/com/studerb/hr/employee/EmployeeDaoTest.java
@@ -1,12 +1,9 @@
 package com.studerb.hr.employee;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.util.List;
 
-import org.apache.commons.lang.SystemUtils;
 import org.hibernate.SessionFactory;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,8 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.NotTransactional;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
-
-import com.studerb.hr.employee.Employee;
 
 @ContextConfiguration(locations = { "classpath:test-context.xml" })
 public class EmployeeDaoTest extends AbstractTransactionalJUnit4SpringContextTests {
@@ -26,33 +21,34 @@ public class EmployeeDaoTest extends AbstractTransactionalJUnit4SpringContextTes
     @Autowired
     SessionFactory sessionFactory;
 
-    //private final String path = SystemUtils.USER_DIR + SystemUtils.FILE_SEPARATOR + "db" + SystemUtils.FILE_SEPARATOR;
-    //String resource = "file:" + path + "test-employeeData.sql";
+    // private final String path = SystemUtils.USER_DIR +
+    // SystemUtils.FILE_SEPARATOR + "db" + SystemUtils.FILE_SEPARATOR;
+    // String resource = "file:" + path + "test-employeeData.sql";
 
     @Before
     public void setUp() throws Exception {
-        logger.debug("*************************************************************SETTING UP*****************************************");
+        this.logger.debug("*************************************************************SETTING UP*****************************************");
     }
 
     @Test
     @NotTransactional
     public void testCreated() {
-        logger.debug("****************CREATED***************");
+        this.logger.debug("****************CREATED***************");
         assertTrue(true);
     }
 
     @Test
     public void getAll() {
-//        Employee employee = new Employee();
-//        employee.setEmail("me@here.com");
-//        employee.setFirstName("Mick");
-//        employee.setLastName("Smith");
+        // Employee employee = new Employee();
+        // employee.setEmail("me@here.com");
+        // employee.setFirstName("Mick");
+        // employee.setLastName("Smith");
 
-        int initialCount = countRowsInTable(employeeDao.getTableName());
-        logger.debug("LOGGING*********************************************");
-        List<Employee> employees = employeeDao.getAll();
-        for(Employee e: employees){
-            logger.debug(e);
+        int initialCount = countRowsInTable(this.employeeDao.getTableName());
+        this.logger.debug("LOGGING*********************************************");
+        List<Employee> employees = this.employeeDao.getAll();
+        for (Employee e : employees) {
+            this.logger.debug(e);
         }
     }
 }
