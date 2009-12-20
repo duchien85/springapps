7867df01b6791ffb47cee46b7630b98aecf29cdb
stbill79 <stbill79@1cf645f8-9684-11dd-b7a0-47679018daad>
2009-12-20 09:22:11 +0000

    added JobHistory model object, primary key object (via IntelliJ) and hibernate mapping file added schema creation sql file

--- hr-base/hr-rest-service/src/test/java/com/studerb/hr/employee/EmployeeDaoTest.java
+++ hr-base/hr-rest-service/src/test/java/com/studerb/hr/employee/EmployeeDaoTest.java
@@ -1,18 +1,21 @@
 package com.studerb.hr.employee;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import java.util.List;
 
 import org.hibernate.SessionFactory;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.annotation.NotTransactional;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+import org.springframework.test.context.transaction.TransactionConfiguration;
 
 @ContextConfiguration(locations = { "classpath:test-context.xml" })
+@TransactionConfiguration(defaultRollback = false)
 public class EmployeeDaoTest extends AbstractTransactionalJUnit4SpringContextTests {
 
     @Autowired
@@ -21,28 +24,45 @@ public class EmployeeDaoTest extends AbstractTransactionalJUnit4SpringContextTes
     @Autowired
     SessionFactory sessionFactory;
 
-    // private final String path = SystemUtils.USER_DIR +
-    // SystemUtils.FILE_SEPARATOR + "db" + SystemUtils.FILE_SEPARATOR;
-    // String resource = "file:" + path + "test-employeeData.sql";
+    static final int TOTAL_COUNT = 107;
 
     @Before
     public void setUp() throws Exception {
-        this.logger.debug("*************************************************************SETTING UP*****************************************");
+        this.simpleJdbcTemplate.update("call reset_hr_dev()", new Object[] {});
     }
 
+    @Test
+    public void countRows() {
+        assertEquals(countRowsInTable(this.employeeDao.getTableName()), TOTAL_COUNT);
+    }
 
     @Test
     public void getAll() {
+        List<Employee> employees = this.employeeDao.getAll();
+        assertEquals(employees.size(), TOTAL_COUNT);
+    }
+
+    @Test
+    public void getOne() {
+        Employee employee = this.employeeDao.get(189L);
+        assertNotNull(employee);
+        assertEquals(employee.getFirstName(), "Jennifer");
+        assertEquals(employee.getLastName(), "Dilly");
+        assertEquals(employee.getEmail(), "JDILLY");
+        assertEquals(employee.getHireDate(), new DateTime(1997, 8, 13, 0, 0, 0, 0, DateTimeZone.forID("America/New_York")));
+    }
+
+    @Test
+    public void deleteOne() {
+        this.employeeDao.delete(101L);
+    }
+
+    @Test
+    public void addEmployee() {
         // Employee employee = new Employee();
         // employee.setEmail("me@here.com");
         // employee.setFirstName("Mick");
         // employee.setLastName("Smith");
-
-        int initialCount = countRowsInTable(this.employeeDao.getTableName());
-        this.logger.debug("LOGGING*********************************************");
-        List<Employee> employees = this.employeeDao.getAll();
-        for (Employee e : employees) {
-            this.logger.debug(e);
-        }
+        assertTrue(true);
     }
 }
