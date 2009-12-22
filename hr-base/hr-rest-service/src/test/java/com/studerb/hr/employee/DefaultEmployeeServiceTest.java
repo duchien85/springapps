6f54333063b8e2ee78ddc2c789a60ad972f5e0f8
stbill79 <stbill79@1cf645f8-9684-11dd-b7a0-47679018daad>
2009-12-22 02:29:33 +0000

    Got rid of Joda-Time remnants update poms to include Jersey json and standard version in basr pom.xml fixed error in properties files (referring to dev db in non-test context)

--- hr-base/hr-rest-service/src/test/java/com/studerb/hr/employee/DefaultEmployeeServiceTest.java
+++ hr-base/hr-rest-service/src/test/java/com/studerb/hr/employee/DefaultEmployeeServiceTest.java
@@ -1,11 +1,10 @@
 package com.studerb.hr.employee;
 
+
 import static org.junit.Assert.*;
 
 import java.util.List;
 
-import org.joda.time.DateTime;
-import org.joda.time.DateTimeZone;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,15 +30,5 @@ public class DefaultEmployeeServiceTest extends AbstractTransactionalJUnit4Sprin
     public void getAll() {
         List<Employee> employees = this.employeeService.getAll();
         assertEquals(employees.size(), TOTAL_COUNT);
-    }
-
-    @Test
-    public void getOne() {
-        Employee employee = this.employeeService.get(189L);
-        assertNotNull(employee);
-        assertEquals(employee.getFirstName(), "Jennifer");
-        assertEquals(employee.getLastName(), "Dilly");
-        assertEquals(employee.getEmail(), "JDILLY");
-        assertEquals(employee.getHireDate(), new DateTime(1997, 8, 13, 0, 0, 0, 0, DateTimeZone.forID("America/New_York")));
     }
 }
