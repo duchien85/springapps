da0dd89c43580f3a12ac4f535fb1f67932b1598c
stbill79 <stbill79@1cf645f8-9684-11dd-b7a0-47679018daad>
2009-12-27 23:29:31 +0000

    added hr-model xml schema test files modifed EmployeeService so as to provide service methods for all classes under com.studerb.hr.employee instead of just the Employee.class itself.

--- hr-base/hr-rest-service/src/test/java/com/studerb/hr/employee/DefaultEmployeeServiceTest.java
+++ hr-base/hr-rest-service/src/test/java/com/studerb/hr/employee/DefaultEmployeeServiceTest.java
@@ -1,7 +1,6 @@
 package com.studerb.hr.employee;
 
-
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.util.List;
 
@@ -28,7 +27,22 @@ public class DefaultEmployeeServiceTest extends AbstractTransactionalJUnit4Sprin
 
     @Test
     public void getAll() {
-        List<Employee> employees = this.employeeService.getAll();
+        List<Employee> employees = this.employeeService.getAllEmployees();
         assertEquals(employees.size(), TOTAL_COUNT);
+    }
+
+    @Test
+    public void getForeignIds() {
+        Employee e = this.employeeService.getEmployee(189L);
+        this.logger.debug("department class: " + e.getDepartment().getClass().getName());
+        this.logger.debug("manager class: " + e.getManager().getClass().getName());
+        this.logger.debug("job class: " + e.getJob().getClass().getName());
+        assertEquals(e.getDepartment().getId(), new Long(50L));
+        assertEquals(e.getManager().getId(), new Long(122L));
+        assertEquals(e.getJob().getId(), "SH_CLERK");
+        this.logger.debug("====================== GETTING NON-ID Foreign Relationships ===========================");
+        assertEquals(e.getDepartment().getName(), "Shipping");
+        assertEquals(e.getJob().getTitle(), "Shipping Clerk");
+        assertEquals(e.getManager().getLastName(), "Kaufling");
     }
 }
