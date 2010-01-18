daaee99b009bec477c321647247c49f99a53bd38
stbill79 <stbill79@1cf645f8-9684-11dd-b7a0-47679018daad>
2010-01-18 05:25:07 +0000

    Starting hr-client webapp using Spring MVC, Sitemesh, Ext JS, etc.

---
+++ hr-base/hr-client/src/main/java/com/studerb/hr/controller/WelcomeController.java
@@ -0,0 +1,19 @@
+package com.studerb.hr.controller;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/welcome")
+public class WelcomeController {
+    Logger logger = Logger.getLogger(WelcomeController.class);
+
+    @RequestMapping(method = RequestMethod.GET)
+    public void welcome(ModelMap model) {
+        logger.debug("Welcome Controller");
+        model.addAttribute("modelMessage", "Hello World!");
+    }
+}
